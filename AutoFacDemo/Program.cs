a5f629c434f92b26900d7164b638c35775d340e5
Marco António <marco_antonio_099@hotmail.com>
2022-01-01 16:01:43 +0000

    Add project files.

---
+++ AutoFacDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoFacDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
