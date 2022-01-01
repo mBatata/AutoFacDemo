eb7b1117ff7f4e3fdda82e346ef1ecfe211d861c
Marco António <marco_antonio_099@hotmail.com>
2022-01-01 17:02:44 +0000

    Added Logger Class & Interface

---
+++ AutoFacDemo.BusinessLogic/Classes/ConsoleLogger.cs
@@ -0,0 +1,12 @@
+﻿using AutoFacDemo.BusinessLogic.Interfaces;
+
+namespace AutoFacDemo.BusinessLogic.Classes
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(string content)
+        {
+            System.Console.WriteLine($"Logging {content}");
+        }
+    }
+}
--- AutoFacDemo.BusinessLogic/Classes/TodayWriter.cs
+++ AutoFacDemo.BusinessLogic/Classes/TodayWriter.cs
@@ -6,15 +6,19 @@ namespace AutoFacDemo.BusinessLogic.Classes
     public class TodayWriter : IDateWriter
     {
         private IOutput _output;
+        private ILogger _logger;
 
-        public TodayWriter(IOutput output)
+        public TodayWriter(IOutput output, ILogger logger)
         {
             _output = output;
+            _logger = logger;
         }
 
         public void WriteDate()
         {
+            _logger.Log($"{nameof(WriteDate)} START");
             _output.Write(DateTime.Today.ToShortDateString());
+            _logger.Log($"{nameof(WriteDate)} END");
         }
     }
 }
