339242ae723b9a3f499446828cf582740f2e47ed
Marco António <marco_antonio_099@hotmail.com>
2022-01-01 22:59:28 +0000

    DateWriterType

--- AutoFacDemo.BusinessLogic/Enums/DateWriterType.cs
+++ AutoFacDemo.BusinessLogic/Enums/DateWriterType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AutoFacDemo.BusinessLogic.Enums
+﻿namespace AutoFacDemo.BusinessLogic.Enums
 {
     public enum DateWriterType
     {
